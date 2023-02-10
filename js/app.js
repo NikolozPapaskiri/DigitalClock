9d9d799c3872b1a62e4dfc9aced297a103e7a6b2
Nikoloz Papaskiri <nkpapaskiri@gmail.com>
2023-02-10 20:00:27 +0400

    api fetch

--- js/app.js
+++ js/app.js
@@ -1,22 +1,4 @@
-let data = new Promise((resolve, reject) => {
-    const users = [`niko`, `valo`, `giga`];
-
-    if( true ) {
-        resolve(users);
-        return;
-    }
-    reject(`There are errors`);
-});
-
-const printUser = async () => {
-    try {
-        const result = await data;
-        console.log(result);
-    } catch (error) {        
-        console.log(error);
-    }
-}
-
-printUser();
-
-// data.then(res => console.log(res)).catch(error => console.log(error));
+fetch('../db/users.json')
+    .then(response => response.json)
+    .then(res => console.log(res))
+    .catch(error => console.log(error));
