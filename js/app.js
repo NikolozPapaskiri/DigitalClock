9182851b49aee1abd7305716d339383bf5a3f7b5
Nikoloz Papaskiri <nkpapaskiri@gmail.com>
2023-02-10 19:22:02 +0400

    Promises

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
-const cars = [`Ferrari`, `lamborghini`, `porsche`];
+let promise = new Promise((resolve, reject) => {
+    if (1 < 2){
+        resolve('success 1');
+        return;
+    }
+    reject('error 1');
+});
 
-let fav = cars.slice(0, 1);
+let promise2 = new Promise((resolve, reject) => {
+    if (1 < 2){
+        resolve('success 2');
+        return;
+    }
+    reject('error 2');
+});
 
-console.log(fav);
+Promise.all([promise, promise2])
+    .then(res => console.log(res))
+    .catch(error => console.log(error));
