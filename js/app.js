d2dbcc847be8c5c96fbbe9bcce5fcb3c8d90a030
Nikoloz Papaskiri <nkpapaskiri@gmail.com>
2023-02-10 22:29:28 +0400

    api fetch done

--- js/app.js
+++ js/app.js
@@ -1,4 +1,16 @@
-fetch('../db/users.json')
-    .then(response => response.json)
-    .then(res => console.log(res))
-    .catch(error => console.log(error));
+const getUsers = async () => {
+    try {
+        const ul = document.querySelector('ul');
+        const users = await fetch('../db/users.json').then(_ => _.json())
+        
+        users.forEach(user => {
+            const li = document.createElement('li');
+            li.innerText = `${user.firstName} ${user.lastname}`;
+            ul.appendChild(li);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+} 
+
+getUsers();
