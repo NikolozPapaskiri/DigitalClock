8366c79112eab89020d624a000643088dceeb7b1
Nikoloz Papaskiri <nkpapaskiri@gmail.com>
2023-02-14 03:00:09 +0400

    spread

--- js/app.js
+++ js/app.js
@@ -1,15 +1,24 @@
-const user = {
-    firstName: `niko`,
-    lastName: `Papa`
+// const sportCars = [`Ferrari`, `Porsche`, `McLaren`];
+// const familyCars = [`Tpyota`, `Honda`, `audi`];
+
+// const allCars = [...sportCars, ...familyCars];
+
+// console.log(allCars);
+
+const car = {
+    name: `Ferrari`,
+    country: `Italy`
+};
+
+const features = {
+    color: `Red`,
+    type: `Hyper car`
+};
+
+const fullCar = {
+    ...car,
+    ...features,
+    price: 500000
 }
 
-// localStorage.setItem(`user`, JSON.stringify(user));
-// localStorage.setItem(`lastName`, `Papaa`);
-
-// localStorage.removeItem(`firstname`);
-
-// localStorage.clear();
-
-const uss = localStorage.getItem(`user`);
-console.log(typeof(uss));
-console.log(JSON.parse(uss).firstName);
+console.log(fullCar);
